b1a24b6bcac0c5e4b72c297513b9a42d1e2bb947
Papillon6814 <41826375+Papillon6814@users.noreply.github.com>
2020-06-11 16:07:36 +0900

    ショートカットキーの割当て

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 var electron = require('electron');
 var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
+const globalShortcut = electron.globalShortcut;
+const dialog = electron.dialog;
 
 // mainWindow変数の初期化
 var mainWindow = null;
@@ -15,6 +17,10 @@ app.on('window-all-closed', function() {
 // 画面を表示．index.htmlを読み込む
 // Close処理を行う
 app.on('ready', function() {
+  globalShortcut.register('Command+T', () => {
+    dialog.showErrorBox("OMG", "CMD+T IS PRESSED");
+  });
+
   // 画面表示
   mainWindow = new BrowserWindow({
     width: 1400, 
@@ -31,8 +37,8 @@ app.on('ready', function() {
     document.addEventListener("copy", e => {
       var copied = window.getSelection().toString();
       console.log(copied);
-    })
-  `)
+    });
+  `);
 
   mainWindow.on('ready-to-show', function () {
     mainWindow.show();
